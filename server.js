80ec0e3e591dee05055eec7ce67c80bc0915d15b
act2fshare-collab <act.2f.share@gmail.com>
2026-05-23 08:09:45 +0900

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const axios = require("axios");
 const app = express();
 app.use(express.json());
 
-const CHANNEL_ACCESS_TOKEN = "ここにアクセストークン";
+const CHANNEL_ACCESS_TOKEN = "R2HMlHOlK7H6zLwFc7MMgNoQoSlBc47zHYyZIUw3GyX/h//FpCe8KJcoyld2e8R+gpSybmFkcmPlBt2MLuHQus2pBciv74kSl1gpBF4csF9qgYXyPXaw1ACv6aWZ+95e6W6CEbBpAvOIeD8oE5iXAgdB04t89/1O/w1cDnyilFU=";
 
 app.post("/webhook", async (req, res) => {
 
