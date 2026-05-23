40f50133305ba0daeb92f78ea2634d728abb565c
act2fshare-collab <act.2f.share@gmail.com>
2026-05-23 16:28:43 +0900

    Update server.js

--- server.js
+++ server.js
@@ -45,6 +45,8 @@ app.post("/webhook", async (req, res) => {
   res.sendStatus(200);
 });
 
-app.listen(3000, () => {
-  console.log("START");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`START PORT ${PORT}`);
 });
